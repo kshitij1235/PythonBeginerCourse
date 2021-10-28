d5fa60de80e6ed0045a4f91d9e82b7369f72ae0b
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-10-28 22:38:51 +0530

    tut7_update

--- tut7.py
+++ tut7.py
@@ -1,17 +1,27 @@
 # sets 
 
-
-set_1=set([1,2])
-set_2=set([2,3,4,5])
+set_1 = set([1,2])
+set_2 = set([2,3,4,5])
 
 print(type(set_1))
 print(set_1)
+# {1, 2}
 
 set_1.add(3)
 print(set_1)
+# {1, 2, 3}    
 
 set_1.add(2)
 print(set_1)
+# {1, 2, 3}
+
+set_1.add(4)
+print(set_1)
+# {1, 2, 3, 4}
+
+set_1.remove(4)
+print(set_1)
+# {1, 2, 3}
 
 print(set_1.union(set_2))
 # {1, 2, 3, 4, 5}
@@ -20,3 +30,4 @@ print(set_1.intersection(set_2))
 # {2, 3}
 
 print(set_1.isdisjoint(set_2))
+# False
