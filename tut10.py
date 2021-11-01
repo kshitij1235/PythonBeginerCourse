54460a715dd8ebd88ddd9233fd76eefd5011cd57
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-11-01 23:51:01 +0530

    Create tut10.py

---
+++ tut10.py
@@ -0,0 +1,38 @@
+# while loops 
+
+condition=1
+count=0
+
+while(condition==1):
+    count+=1
+    if count>10:condition=2
+    else:print("loop is running for ",count)
+
+# loop is running for  1        
+# loop is running for  2        
+# loop is running for  3        
+# loop is running for  4        
+# loop is running for  5        
+# loop is running for  6        
+# loop is running for  7        
+# loop is running for  8        
+# loop is running for  9        
+# loop is running for  10    
+
+count=0
+while True:
+    count+=1
+    if count>10:False
+    else:print(count)
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
