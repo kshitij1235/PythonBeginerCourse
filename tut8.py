f109c407317c1baabb7b029bb448061b99056605
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-11-01 11:25:22 +0530

    tut8

---
+++ tut8.py
@@ -0,0 +1,20 @@
+# if  else 
+
+# if i have 10 ladoos : eat 
+# else : dont eat 
+
+num1=6
+num2=2
+
+# only if there is one line 
+if num1+num2==8:print("yes yes")
+else:print("no")
+# yes yes
+if num1*num2==3:print("its 3")
+elif num1*num2==12:print("its 12")
+# its 12
+else:print("no its not 3")
+# no its not 3
+
+
+
