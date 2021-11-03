3219fc461dad90c0e5dfef8832c51c8fdfc3099a
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-11-03 13:39:45 +0530

    Create tut11.py

---
+++ tut11.py
@@ -0,0 +1,17 @@
+# break and continue 
+
+for val in "panipuri":
+    if val =="i":
+        break
+    print(val)
+    
+for val in "panipuri":
+    if val =="i":
+        continue
+    print(val)
+
+for val in "panipuri":
+    if val =="i":
+        pass
+    print(val)
+    
